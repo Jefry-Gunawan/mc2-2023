9db9a642e49b5cb8189e628a7b70f65c3b1c778c
Jefry Gunawan <gunawanjefry21@yahoo.co.id>
2023-06-21 15:10:19 +0700

    test commit

--- mc2-2023/ContentView.swift
+++ mc2-2023/ContentView.swift
@@ -22,6 +22,7 @@ struct ContentView: View {
                 ForEach(items) { item in
                     NavigationLink {
                         Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text("Hello")
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
