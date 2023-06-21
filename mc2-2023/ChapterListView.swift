88daca3e9dbc1fe7fb9833250ddbed1e5b55541a
Jefry Gunawan <gunawanjefry21@yahoo.co.id>
2023-06-21 16:59:12 +0700

    ADD : ChapterListView

---
+++ mc2-2023/ChapterListView.swift
@@ -0,0 +1,20 @@
+//
+//  ChapterListView.swift
+//  mc2-2023
+//
+//  Created by Jefry Gunawan on 21/06/23.
+//
+
+import SwiftUI
+
+struct ChapterListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChapterListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChapterListView()
+    }
+}
