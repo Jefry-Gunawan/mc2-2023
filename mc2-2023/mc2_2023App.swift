0ba84aa15c09fb1cb6ce5ff576a6a12bc733334c
Jefry Gunawan <gunawanjefry21@yahoo.co.id>
2023-06-21 15:00:28 +0700

    Initial Commit

---
+++ mc2-2023/mc2_2023App.swift
@@ -0,0 +1,20 @@
+//
+//  mc2_2023App.swift
+//  mc2-2023
+//
+//  Created by Jefry Gunawan on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct mc2_2023App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ mc2-2023UITests/mc2_2023UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mc2_2023UITestsLaunchTests.swift
+//  mc2-2023UITests
+//
+//  Created by Jefry Gunawan on 21/06/23.
+//
+
+import XCTest
+
+final class mc2_2023UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
