3cc28e78cabad88f159af8a1ccfebff3b1dd671f
Jefry Gunawan <gunawanjefry21@yahoo.co.id>
2023-06-26 16:07:29 +0700

    ADD SpeechRecognition (Not Done)

---
+++ mc2-2023/Speaking/SpeakingView.swift
@@ -0,0 +1,21 @@
+//
+//  SpeakingView.swift
+//  mc2-2023
+//
+//  Created by Jefry Gunawan on 23/06/23.
+//
+
+import SwiftUI
+
+
+struct SpeakingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpeakingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeakingView()
+    }
+}
---
+++ mc2-2023/Speaking/SpeechRecognition.swift
@@ -0,0 +1,88 @@
+//
+//  SpeechRecognition.swift
+//  mc2-2023
+//
+//  Created by Jefry Gunawan on 26/06/23.
+//
+
+import SwiftUI
+import AVFoundation
+import Speech
+import Foundation
+
+class SpeechRecognizer: ObservableObject {
+    init() {
+        recognizer = SFSpeechRecognizer()
+        
+        Task(priority: .background) {
+            do {
+                guard recognizer != nil else {
+                    throw RecognizerError.nilRecognizer
+                }
+                guard await SFSpeechRecognizer.hasAuthorizationToRecognize() else {
+                    throw RecognizerError.notAuthorizedToRecognize
+                }
+                guard await AVAudioSession.sharedInstance().hasPermissionToRecord() else {
+                    throw RecognizerError.notPermittedToRecord
+                }
+            } catch {
+//                speakError(error)
+            }
+        }
+    }
+    
+    deinit {
+        reset()
+    }
+    
+    func reset() {
+            task?.cancel()
+            audioEngine?.stop()
+            audioEngine = nil
+            request = nil
+            task = nil
+        }
+    
+    enum RecognizerError: Error {
+        case nilRecognizer
+        case notAuthorizedToRecognize
+        case notPermittedToRecord
+        case recognizerIsUnavailable
+        
+        var message: String {
+            switch self {
+            case .nilRecognizer: return "Can't initialize speech recognizer"
+            case .notAuthorizedToRecognize: return "Not authorized to recognize speech"
+            case .notPermittedToRecord: return "Not permitted to record audio"
+            case .recognizerIsUnavailable: return "Recognizer is unavailable"
+            }
+        }
+    }
+    
+    @Published var transcript: String = ""
+        
+    private var audioEngine: AVAudioEngine?
+    private var request: SFSpeechAudioBufferRecognitionRequest?
+    private var task: SFSpeechRecognitionTask?
+    private let recognizer: SFSpeechRecognizer?
+}
+
+extension SFSpeechRecognizer {
+    static func hasAuthorizationToRecognize() async -> Bool {
+        await withCheckedContinuation { continuation in
+            requestAuthorization { status in
+                continuation.resume(returning: status == .authorized)
+            }
+        }
+    }
+}
+
+extension AVAudioSession {
+    func hasPermissionToRecord() async -> Bool {
+        await withCheckedContinuation { continuation in
+            requestRecordPermission { authorized in
+                continuation.resume(returning: authorized)
+            }
+        }
+    }
+}
