564fbfa1dedab69ae48186220380da9d169790a8
Natasya Febriani <s160419080@student.ubaya.ac.id>
2023-07-06 13:52:26 +0700

    FIX : modify so that overlay only appears when app is first launched

--- mc2-2023/Topics/View/TopicsView.swift
+++ mc2-2023/Topics/View/TopicsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TopicsView: View {
     @State var showContent: Bool = true
+    @AppStorage("isFirstLaunch") var isFirstLaunch: Bool = true
     //Gesture Properties
     @State var offsetY: CGFloat = 0
     @State var currentIndex: CGFloat = 0
@@ -86,39 +87,42 @@ struct TopicsView: View {
             .preferredColorScheme(.light)
         }
         .overlay(content: {
-            Button {
-                showContent = false
-                
-            } label: {
+            if isFirstLaunch {
                 Rectangle()
                     .fill(.ultraThinMaterial)
                     .opacity(showContent ? 1 : 0)
                     .ignoresSafeArea()
+                    .onTapGesture {
+                        showContent = false
+                        isFirstLaunch = false // Update isFirstLaunch value
+                    }
+                    .transition(.opacity) // Apply fade animation
             }
         })
         .overlay(content: {
-            VStack {
-                Text("Swipe up and down on the buildings to change topics")
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 25)
+            if isFirstLaunch {
+                VStack {
+                    Text("Swipe up and down on the buildings to change topics")
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 25)
 
-                HStack{
-                    Image("Topic1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 250, height: 250)
-                        .padding(.leading, 40)
-                    Image("updown")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 80, height: 80)
-                        .padding(.leading, -60)
-                        .padding(.top, -100)
-                       
+                    HStack{
+                        Image("Topic1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 250, height: 250)
+                            .padding(.leading, 40)
+                        Image("updown")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .padding(.leading, -60)
+                            .padding(.top, -100)
+                           
+                    }
                 }
+                .opacity(showContent ? 1 : 0)
             }
-            .opacity(showContent ? 1 : 0)
-            
         })
     }
     
