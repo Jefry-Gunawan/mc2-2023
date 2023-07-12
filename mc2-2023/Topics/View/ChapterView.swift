369c209d74bd1d261c2afc176ad9ba9669685a09
Jefry Gunawan <gunawanjefry21@yahoo.co.id>
2023-07-12 16:51:25 +0700

    FIXED Chapter View Data not updating on change

--- mc2-2023/Topics/View/ChapterView.swift
+++ mc2-2023/Topics/View/ChapterView.swift
@@ -7,7 +7,40 @@
 import SwiftUI
 
 struct ChapterView: View {
+    
+    
     var body: some View {
+        var chapterList: [Chapter] = {
+            var chapters: [Chapter] = []
+
+            
+            if let topicId = topicID,
+               let chapterId = chapterID {
+                
+                let filtered1 = chapterDf.filter(on: "topic_id", Int.self, {$0 == topicId})
+                let filtered2 = filtered1.filter(on: "id", Int.self, {$0 == chapterId})
+                
+                for row in filtered2.rows {
+                    if let name = row["name"] as? String,
+                       let description = row["description"] as? String,
+                       let tensesId = row["tenses_id"] as? Int,
+                       let topicId = row["topic_id"] as? Int,
+                       let vocabId = row["vocab_id"] as? String {
+                        
+                        let tensesfilter = tensesDf.filter(on: "id", Int.self, {$0 == tensesId})
+                        
+                        for tensesrow in tensesfilter.rows{
+                            if let tensesname = tensesrow["name"] as? String{
+                                let chapter = Chapter(name: name, description: description, tensesId: tensesId, topicId: topicId, vocabId: vocabId, tensesName: tensesname)
+                                chapters.append(chapter)
+                            }
+                        }
+                    }
+                }
+            }
+            return chapters
+        }()
+        
             VStack {
                 VStack{
                     Rectangle()
